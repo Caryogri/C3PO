3c8b3b5cc46612154e742b4a237f4ea63dea68e6
Griffin Caryotakis <griffrgc@gmail.com>
2023-05-17 17:55:28 -0600

    Add script to streamline package install for running on local machines.

---
+++ installDependancies.R
@@ -0,0 +1,12 @@
+requiredPackages = c("remotes", "shiny", "shinyjs", "stringr", "yaml",
+                    "data.table", "Matrix", "splitstackshape",
+                    "r2d3", "jsonlite", "rsvg", "aws.s3")
+
+installed_packages <- requiredPackages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+if(!"Rutilitybelt" %in% installed.packages()) {
+  remotes::install_github('qdread/Rutilitybelt')
+}
